29265638bc7b40e58e3d46e60830cb8959fd7283
theoadepaula <theoadepaula@gmail.com>
2022-12-20 21:44:00 -0300

    Aperfeiçoando as funções e colocando github actions

---
+++ tests/testthat/test-test_funcoes.R
@@ -0,0 +1,130 @@
+test_that("calcular_capital_juros_composto não funciona", {
+
+  expect_error(calcular_capital_juros_composto())
+  expect_error(calcular_capital_juros_composto(1,1,'1'))
+  expect_error(calcular_capital_juros_composto(1,'1','1'))
+  expect_error(calcular_capital_juros_composto('1','1','1'))
+  expect_error(calcular_capital_juros_composto('1',1,'1'))
+  expect_error(calcular_capital_juros_composto('1','1',1))
+  expect_error(calcular_capital_juros_composto('1',1,1))
+  expect_error(calcular_capital_juros_composto(1,'1',1))
+  expect_error(calcular_capital_juros_composto('1','1',1))
+})
+
+
+test_that("calcular_capital_juros_simples não funciona", {
+
+  expect_error(calcular_capital_juros_simples())
+  expect_error(calcular_capital_juros_simples(1,1,'1'))
+  expect_error(calcular_capital_juros_simples(1,'1','1'))
+  expect_error(calcular_capital_juros_simples('1','1','1'))
+  expect_error(calcular_capital_juros_simples('1',1,'1'))
+  expect_error(calcular_capital_juros_simples('1','1',1))
+  expect_error(calcular_capital_juros_simples('1',1,1))
+  expect_error(calcular_capital_juros_simples(1,'1',1))
+  expect_error(calcular_capital_juros_simples('1','1',1))
+})
+
+test_that("calcular_juros_composto não funciona", {
+
+  expect_error(calcular_juros_composto())
+  expect_error(calcular_juros_composto(1,1,'1'))
+  expect_error(calcular_juros_composto(1,'1','1'))
+  expect_error(calcular_juros_composto('1','1','1'))
+  expect_error(calcular_juros_composto('1',1,'1'))
+  expect_error(calcular_juros_composto('1','1',1))
+  expect_error(calcular_juros_composto('1',1,1))
+  expect_error(calcular_juros_composto(1,'1',1))
+  expect_error(calcular_juros_composto('1','1',1))
+})
+
+test_that("calcular_juros_simples não funciona", {
+
+  expect_error(calcular_juros_simples())
+  expect_error(calcular_juros_simples(1,1,'1'))
+  expect_error(calcular_juros_simples(1,'1','1'))
+  expect_error(calcular_juros_simples('1','1','1'))
+  expect_error(calcular_juros_simples('1',1,'1'))
+  expect_error(calcular_juros_simples('1','1',1))
+  expect_error(calcular_juros_simples('1',1,1))
+  expect_error(calcular_juros_simples(1,'1',1))
+  expect_error(calcular_juros_simples('1','1',1))
+})
+
+test_that("calcular_montante_juros_composto não funciona", {
+
+  expect_error(calcular_montante_juros_composto())
+  expect_error(calcular_montante_juros_composto(1,1,'1'))
+  expect_error(calcular_montante_juros_composto(1,'1','1'))
+  expect_error(calcular_montante_juros_composto('1','1','1'))
+  expect_error(calcular_montante_juros_composto('1',1,'1'))
+  expect_error(calcular_montante_juros_composto('1','1',1))
+  expect_error(calcular_montante_juros_composto('1',1,1))
+  expect_error(calcular_montante_juros_composto(1,'1',1))
+  expect_error(calcular_montante_juros_composto('1','1',1))
+})
+
+test_that("calcular_montante_juros_simples não funciona", {
+
+  expect_error(calcular_montante_juros_simples())
+  expect_error(calcular_montante_juros_simples(1,1,'1'))
+  expect_error(calcular_montante_juros_simples(1,'1','1'))
+  expect_error(calcular_montante_juros_simples('1','1','1'))
+  expect_error(calcular_montante_juros_simples('1',1,'1'))
+  expect_error(calcular_montante_juros_simples('1','1',1))
+  expect_error(calcular_montante_juros_simples('1',1,1))
+  expect_error(calcular_montante_juros_simples(1,'1',1))
+  expect_error(calcular_montante_juros_simples('1','1',1))
+})
+
+test_that("calcular_taxa_juros_composto não funciona", {
+
+  expect_error(calcular_taxa_juros_composto())
+  expect_error(calcular_taxa_juros_composto(1,1,'1'))
+  expect_error(calcular_taxa_juros_composto(1,'1','1'))
+  expect_error(calcular_taxa_juros_composto('1','1','1'))
+  expect_error(calcular_taxa_juros_composto('1',1,'1'))
+  expect_error(calcular_taxa_juros_composto('1','1',1))
+  expect_error(calcular_taxa_juros_composto('1',1,1))
+  expect_error(calcular_taxa_juros_composto(1,'1',1))
+  expect_error(calcular_taxa_juros_composto('1','1',1))
+})
+
+test_that("calcular_taxa_juros_simples não funciona", {
+
+  expect_error(calcular_taxa_juros_simples())
+  expect_error(calcular_taxa_juros_simples(1,1,'1'))
+  expect_error(calcular_taxa_juros_simples(1,'1','1'))
+  expect_error(calcular_taxa_juros_simples('1','1','1'))
+  expect_error(calcular_taxa_juros_simples('1',1,'1'))
+  expect_error(calcular_taxa_juros_simples('1','1',1))
+  expect_error(calcular_taxa_juros_simples('1',1,1))
+  expect_error(calcular_taxa_juros_simples(1,'1',1))
+  expect_error(calcular_taxa_juros_simples('1','1',1))
+})
+
+test_that("calcular_tempo_juros_composto não funciona", {
+
+  expect_error(calcular_tempo_juros_composto())
+  expect_error(calcular_tempo_juros_composto(1,1,'1'))
+  expect_error(calcular_tempo_juros_composto(1,'1','1'))
+  expect_error(calcular_tempo_juros_composto('1','1','1'))
+  expect_error(calcular_tempo_juros_composto('1',1,'1'))
+  expect_error(calcular_tempo_juros_composto('1','1',1))
+  expect_error(calcular_tempo_juros_composto('1',1,1))
+  expect_error(calcular_tempo_juros_composto(1,'1',1))
+  expect_error(calcular_tempo_juros_composto('1','1',1))
+})
+
+test_that("calcular_tempo_juros_simples não funciona", {
+
+  expect_error(calcular_tempo_juros_simples())
+  expect_error(calcular_tempo_juros_simples(1,1,'1'))
+  expect_error(calcular_tempo_juros_simples(1,'1','1'))
+  expect_error(calcular_tempo_juros_simples('1','1','1'))
+  expect_error(calcular_tempo_juros_simples('1',1,'1'))
+  expect_error(calcular_tempo_juros_simples('1','1',1))
+  expect_error(calcular_tempo_juros_simples('1',1,1))
+  expect_error(calcular_tempo_juros_simples(1,'1',1))
+  expect_error(calcular_tempo_juros_simples('1','1',1))
+})
